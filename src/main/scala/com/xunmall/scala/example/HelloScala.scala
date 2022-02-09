650e9a8873ed0db71a8785be42ae506d900a4abe
wangyj03 <wangyj03@zenmen.com>
2022-02-09 15:14:27 +0800

    init

---
+++ src/main/scala/com/xunmall/scala/example/HelloScala.scala
@@ -0,0 +1,14 @@
+package com.xunmall.scala.example
+
+/**
+  * @description
+  * @author wangyj03@zenmen.com
+  * @date 2021/1/13 9:08 
+  */
+object HelloScala {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello scala...")
+  }
+
+}
---
+++ src/main/scala/com/xunmall/scala/example/LookSourceCode.scala
@@ -0,0 +1,17 @@
+package com.xunmall.scala.example
+
+/**
+  * @description
+  * @author wangyj03@zenmen.com
+  * @date 2021/1/13 9:10 
+  */
+object LookSourceCode {
+
+  def main(args: Array[String]): Unit = {
+    var arr = new Array[Int](10)
+    for (itme <- arr) {
+      println("item=" + itme)
+    }
+  }
+
+}
